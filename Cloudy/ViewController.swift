4e01f958cb031b506e906d978d8f9b001d2df9d3
Serhii Kovtunenko <s.kovtunenko@eurospektras.lt>
2020-06-14 16:08:43 +0300

    Initial commit

---
+++ Cloudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cloudy
+//
+//  Created by Serhii Kovtunenko on 14.06.2020.
+//  Copyright © 2020 Serhii Kovtunenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
